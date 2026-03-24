21357252ecb62b257c56742e906d85fc6b07cb3b
Lahiru Weliwitiya <lahiruweliwitiya@hotmail.com>
2026-03-24 13:09:26 +0530

    main shell code cleanup

--- doodlevox_mobile/lib/screens/shared/dv_main_shell.dart
+++ doodlevox_mobile/lib/screens/shared/dv_main_shell.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:doodlevox_mobile/widgets/dv_logo.dart';
-// import 'package:doodlevox_mobile/styles/dv_bottom_nav_style.dart';
 
 class DVMainShell extends StatelessWidget {
   const DVMainShell({
@@ -13,20 +12,11 @@ class DVMainShell extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    // final bottomTheme = Theme.of(context).bottomNavigationBarTheme;
-    // final colorScheme = Theme.of(context).colorScheme;
-
     return Scaffold(
       appBar: AppBar(title: const DVLogo(size: 40)),
       body: navigationShell,
       bottomNavigationBar: BottomNavigationBar(
         currentIndex: navigationShell.currentIndex,
-        // type: bottomTheme.type ?? BottomNavigationBarType.fixed,
-        // backgroundColor: bottomTheme.backgroundColor ?? colorScheme.surface,
-        // selectedItemColor: bottomTheme.selectedItemColor ?? colorScheme.primary,
-        // unselectedItemColor: bottomTheme.unselectedItemColor ?? colorScheme.onSurface.withValues(alpha: 0.6),
-        // selectedLabelStyle: bottomTheme.selectedLabelStyle,
-        // unselectedLabelStyle: bottomTheme.unselectedLabelStyle,
         onTap: (index) {
           navigationShell.goBranch(
             index,
